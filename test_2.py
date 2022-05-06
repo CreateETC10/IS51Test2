fa4df00b08beba517818628d3c93dd6ebd76df2b
CreateETC10 <greene_10@mail.fresnostate.edu>
2022-05-05 22:55:01 -0700

    finished Structured-English

--- test_2.py
+++ test_2.py
@@ -8,7 +8,13 @@ The third item it will display is the percentage of grades that are above the av
 First we will establish a connection to the file
 Then the program will read the file in a loop
 Then we will close the file
-There will be 3 functions, the first will display the number of grades
-The second will display the average grade
-The third will display the percentage of grades above the average grade
+There will be 4 functions, the first will point the program to a file and read the file
+The second will display the number of grades
+The third will display the average grade
+The fourth will display the percentage of grades above the average grade
+"""
+
+
+"""
+
 """
