5b93c9cf796af9493ca632f4696f0682643beadc
CreateETC10 <greene_10@mail.fresnostate.edu>
2022-05-05 22:41:08 -0700

    Finish Structured-English

---
+++ test_2.py
@@ -0,0 +1,14 @@
+"""
+The program is trying to analyze the grades on a final exam.
+The program will point to a file, Final.txt, and analyze it and display 3 items to the user.
+The first item that it will display is the number of grades.
+The second item that will display is the average grade.
+The third item it will display is the percentage of grades that are above the average grade.
+
+First we will establish a connection to the file
+Then the program will read the file in a loop
+Then we will close the file
+There will be 3 functions, the first will display the number of grades
+The second will display the average grade
+The third will display the percentage of grades above the average grade
+"""
