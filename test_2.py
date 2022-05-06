b2de2cfb8a4ebcb6339361ec6ae680edb3087d8f
CreateETC10 <greene_10@mail.fresnostate.edu>
2022-05-06 00:27:53 -0700

    finish exam-end

--- test_2.py
+++ test_2.py
@@ -40,5 +40,18 @@ num = 0 # number of grades above average
  num += 1
  output is the percentage of grades above the average grade
 
-main
 """
+
+infile = open("Final (1).txt", 'r')
+grades = [line.rstrip() for line in infile]
+infile.close()
+for i in range(len(grades)):
+    grades[i] = int(grades[i])
+average = sum(grades) / len(grades)
+num = 0 
+for grade in grades:
+    if grade > average:
+        num += 1
+print("Number of grades:", len(grades))
+print("Average grade:", average)
+print("Percentage of grades above average: {0:.2f}%".format(100 * num / len(grades)))
